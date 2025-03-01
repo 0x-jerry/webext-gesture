8c6bc5f44632907a32a39aa33f12ad539a1e677e
Jerry <x.jerry.wang@gmail.com>
2025-03-01 16:37:22 +0800

    perf(content): move content script execution to document_end

--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -2,7 +2,7 @@ import App from './App.vue'
 
 export default defineContentScript({
   matches: ['<all_urls>'],
-  runAt: 'document_start',
+  runAt: 'document_end',
   cssInjectionMode: 'ui',
   async main(ctx) {
     const ui = await createShadowRootUi(ctx, {
