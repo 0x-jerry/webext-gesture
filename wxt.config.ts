5d4f2477278435e053a7724c249824a6146f613e
Jerry <x.jerry.wang@gmail.com>
2025-11-20 03:17:01 +0000

    perf: upgrade deps

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
   modules: ['@wxt-dev/module-vue'],
-  runner: {
+  webExt: {
     chromiumProfile: '.profile/chrome',
     keepProfileChanges: true,
     startUrls: ['https://example.com'],
